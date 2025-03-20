d4d18827e5da47eed50ec55991844263c3373853
Aryan <aryan.bagane@dataeaze.io>
2025-03-20 23:18:57 +0530

    int and float datatypes

---
+++ 3_python_basics/data_types.py
@@ -0,0 +1,15 @@
+# Fundamental data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# classes -> custom types such as self made (for eg: SuperCar)
+
+# specialised data types -> modules and packages
+
+None   # means nothing
---
+++ 3_python_basics/python_numbers.py
@@ -0,0 +1,17 @@
+# Fundamental data types
+
+# Integer and Float
+print(2 + 4)
+print(2 * 4)
+print(2 - 4)
+print(2 / 4)
+print(type(2 + 4))
+print(type(2 / 4))
+print(type(0))
+print(type(0.0))
+print(2 ** 6)
+print(5 // 2)
+print(5 % 2)
+print(type(2 ** 6))
+print(type(5 // 2))
+print(type(5 % 2))
