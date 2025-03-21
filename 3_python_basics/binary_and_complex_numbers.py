2abc1be5453f3ce1226ad8be22edaf4dbc3b139e
Aryan <aryan.bagane@dataeaze.io>
2025-03-21 23:16:20 +0530

    bin and complex, and operator precedence

---
+++ 3_python_basics/binary_and_complex_numbers.py
@@ -0,0 +1,5 @@
+# Create a complex number from a real part and an optional imaginary part. This is equivalent to (real + imag*1j) where imag defaults to 0.
+complex
+
+# Return the binary representation of an integer.
+print(bin(69))
---
+++ 3_python_basics/operator_precedence.py
@@ -0,0 +1,3 @@
+# operator_precedence.py
+# we follow the precedence -> () -> ** -> * / -> + -
+print(3 + 2 * 3)
